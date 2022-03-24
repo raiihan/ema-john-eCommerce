4ceee4820e91c0fc63581437ab5accfc16502dd6
silvaan <s.siilvan@yahoo.com>
2022-03-24 12:36:51 +0600

    Product components added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import './Product.css'
 
-const Product = () => {
+const Product = ({ product }) => {
+    const { img, name, seller, ratings, price, id } = product;
     return (
-        <div>
-            <h1>Product</h1>
+        <div className='cart-container'>
+            <img src={img} alt="Product" />
+            <div className="product-info">
+                <h3>Name: {name}</h3>
+                <p>Price: ${price}</p>
+                <p><small>Manufacturer: {seller}</small></p>
+                <p><small>Ratting: {ratings} stars</small></p>
+            </div>
+            <button className='btn-cart'>
+                <p>Add to Cart</p>
+                <FontAwesomeIcon icon={faShoppingCart} />
+            </button>
         </div>
     );
 };
